6c0ab532b37579a71c98398aa37b543a9818e2d5
slamke <sunke3296@gmail.com>
2014-07-05 16:58:19 +0800

    remove class file

--- SMSStattion/src/com/unityprima/smsstattion/webservice/LoaderWebService.java
+++ SMSStattion/src/com/unityprima/smsstattion/webservice/LoaderWebService.java
@@ -1,5 +1,56 @@
 package com.unityprima.smsstattion.webservice;
 
-public class LoaderWebService {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.unityprima.smsstattion.entity.SMSWSend;
+import com.unityprima.smsstattion.utils.ClassParse;
+import com.unityprima.smsstattion.utils.MD5;
+import com.unityprima.smsstattion.utils.Message;
+/**
+ * 对应于短信发送模块，使用httpclient访问服务器的webservice，将数据load至手机
+ * load的数据为List<SMSWSend>对应的json格式字符串，使用gson进行解析
+ * @author sunke
+ *
+ */
+public class LoaderWebService  extends BasicWebService{
+	private final String url = "service/send/list";
+	private Context _context;
+	public LoaderWebService(Context _context) {
+		super();
+		this._context = _context;
+	}
+
+	public List<SMSWSend> getSMSSendList() {
+		SharedPreferences settingInfo = _context.getSharedPreferences(Message.PREFERENCE_NAME, Context.MODE_PRIVATE); 
+        String severAddress = settingInfo.getString(Message.SEVER_ADDRESS, "");
+        String _url = null;
+		if (severAddress.endsWith("/")) {
+			_url = severAddress+url;
+		}else {
+			_url = severAddress+"/"+url;
+		}
+		String code = Calendar.YEAR+"-"+Calendar.MONTH+"-"+Calendar.DAY_OF_MONTH+"unityprima";
+		String key = new MD5().getMD5Str(code);
+		Map<String, String> map = new HashMap<String, String>();
+		map.put(Message.KEY, key);
+		//result可能为：Message.SUCCESS--Message.ERROR--Message.NETWORK_FAIL--content
+		String result = sendPostRequest(_url, map); 
+		List<SMSWSend> list = new ArrayList<SMSWSend>();
+		//表示网络错误或者服务器错误
+		if (result.equals(Message.NETWORK_FAIL) ||result.equals(Message.ERROR)) {
+			return null; //出现错误
+ 		}else if(result.equals(Message.SUCCESS) ){  //表示当前没有待发送短信
+			return list; //size ==0
+		}else {
+			//使用gson将string转换为对象
+			return new ClassParse().string2SMSWSendList(result);
+		}
+	}
 }
--- SMSStattion/src/com/unityprima/smsstattion/webservice/UploadWebService.java
+++ SMSStattion/src/com/unityprima/smsstattion/webservice/UploadWebService.java
@@ -1,5 +1,47 @@
 package com.unityprima.smsstattion.webservice;
 
-public class UploadWebService {
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.unityprima.smsstattion.entity.SMSMO;
+import com.unityprima.smsstattion.utils.ClassParse;
+import com.unityprima.smsstattion.utils.MD5;
+import com.unityprima.smsstattion.utils.Message;
+/**
+ * 对应于短信接收模块，使用httpclient访问服务器的webservice，将数据upload至服务器
+ * upload的数据为ArrayList<SMSMO>对应的json格式字符串，使用gson进行解析后发送
+ * @author sunke
+ *
+ */
+public class UploadWebService extends BasicWebService{
+	private final String url = "service/receive/items";
+	private Context _context;
+	public UploadWebService(Context _context) {
+		super();
+		this._context = _context;
+	}
+
+	public String sendSMSMOList(List<SMSMO> list) {
+		SharedPreferences settingInfo = _context.getSharedPreferences(Message.PREFERENCE_NAME, Context.MODE_PRIVATE); 
+        String severAddress = settingInfo.getString(Message.SEVER_ADDRESS, "");
+        String _url = null;
+		if (severAddress.endsWith("/")) {
+			_url = severAddress+url;
+		}else {
+			_url = severAddress+"/"+url;
+		}
+		String code = Calendar.YEAR+"-"+Calendar.MONTH+"-"+Calendar.DAY_OF_MONTH+"unityprima";
+		String key = new MD5().getMD5Str(code);
+		String content = new ClassParse().object2String(list);
+		Map<String, String> map = new HashMap<String, String>();
+		map.put(Message.KEY, key);
+		map.put(Message.CONTENT, content);
+		//可能的结果为//Message.SUCCESS（访问成功）--Message.ERROR（服务器错误）--Message.NETWORK_FAIL（网络错误）
+		return sendPostRequest(_url, map); 
+	}
 }
